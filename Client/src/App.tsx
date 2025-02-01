6baa2613d8dac48c9cda20d0d9ae2bf5fa4d0edc
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2025-02-01 17:44:11 +0300

    Add sample product data and update ProductList to display active products

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,3 +1,9 @@
+const products = [
+  { id: 1, name: 'Product 1', price: 100, isActive: true },
+  { id: 2, name: 'Product 2', price: 200, isActive: true },
+  { id: 3, name: 'Product 3', price: 300, isActive: false },
+]
+
 function App() {
   return (
     <>
@@ -15,17 +21,19 @@ function ProductList() {
   return (
     <>
       <h2>Product List</h2>
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
+      {products.map((p) => p.isActive && <Product key={p.id} products={p} />)}
     </>
   )
 }
 
-function Product() {
-  return <h3>Product</h3>
+function Product(props: any) {
+  return (
+    <>
+      <h3>
+        {props.products.name} - <b>{props.products.price}</b>
+      </h3>
+    </>
+  )
 }
 
 export default App
