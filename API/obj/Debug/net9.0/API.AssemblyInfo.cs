1c7229a8eac90be744d7682988a28ed261430a70
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2025-02-03 09:48:59 +0300

    Add ButtonUsage component and integrate Material UI; import Roboto font styles

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4844226f2a08a4f571ed4497dbdadbaa4793021a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e3a906ac4e70dd342993cf3ed356d095b9a5744")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
