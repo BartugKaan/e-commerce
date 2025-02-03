313f8019fb075e3acef115692611b8dd99f31ba3
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2025-02-03 10:14:09 +0300

    Add Porduct images to wwwroot

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a7b271dbfcf1b6bf7f3a6e50b6b207d6f70f6ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0356251ac630e2d12f7cfc9a48b335fd052931e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
