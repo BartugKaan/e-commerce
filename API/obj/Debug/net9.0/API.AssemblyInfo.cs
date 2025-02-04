3742ba95a3d52a81bad41216345f4d62e7ddfe74
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2025-02-04 15:15:08 +0300

    Update assembly informational version and sourcelink for API

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa15030ba131154627f72cbfb68dc7bca3ffaaf6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b46dc69b5571a3c9dfd5ef5c3b6dda5177baf1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
