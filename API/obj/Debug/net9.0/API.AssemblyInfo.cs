89a6344d1fa3c90a6f836171aff913181cc2c446
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2025-02-04 17:20:28 +0300

    Add realistic data for dabatase system

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3742ba95a3d52a81bad41216345f4d62e7ddfe74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f768dbb3a3bbefedf590464bea699a9b3f22495")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
